ebffa05829bd1a88398553f8e22ac53c74837d3e
moon-zhou <ayimin1989@163.com>
2022-06-23 10:49:09 +0800

    feat: https no cer

--- spring-boot-httputil/src/main/java/org/moonzhou/httputil/config/RestTemplateConfig.java
+++ spring-boot-httputil/src/main/java/org/moonzhou/httputil/config/RestTemplateConfig.java
@@ -1,19 +1,53 @@
 package org.moonzhou.httputil.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.TrustStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import java.security.cert.X509Certificate;
 
 /**
  * @author moon zhou
  */
+@Slf4j
 @Configuration
 public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
         return new RestTemplate(factory);
+    }
+
+    @Bean
+    public RestTemplate restTemplateCer() {
+        HttpComponentsClientHttpRequestFactory requestFactory =
+                new HttpComponentsClientHttpRequestFactory();
+        try {
+            TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+            SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
+                    .loadTrustMaterial(null, acceptingTrustStrategy)
+                    .build();
+            SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+            CloseableHttpClient httpClient = HttpClients.custom()
+                    .setSSLSocketFactory(csf)
+                    .build();
+
+            requestFactory.setHttpClient(httpClient);
+            requestFactory.setReadTimeout(5000);
+            requestFactory.setConnectTimeout(15000);
+        } catch (Exception e) {
+            log.error("init restTemplate error: ", e);
+        }
+
+        return new RestTemplate(requestFactory);
     }
 
     @Bean
