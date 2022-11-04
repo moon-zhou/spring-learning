71ac39a4226ca4bc4e8541f389ebb05385b3ddc1
moon-zhou <ayimin1989@163.com>
2022-11-04 11:19:15 +0800

    feat: springboot transaction
    sub service with transaction, entry method with transaction

--- spring-boot-transaction/src/test/java/org/moonzhou/transaction/service/impl/SignBizServiceImplTest.java
+++ spring-boot-transaction/src/test/java/org/moonzhou/transaction/service/impl/SignBizServiceImplTest.java
@@ -97,12 +97,69 @@ public class SignBizServiceImplTest {
         SignRetry signRetryCaseFour = signRetryService.getOneByParam(signRetryParam);
         assertNotNull(signCaseFour);
         assertNotNull(signRetryCaseFour);
+
+        /////////////////////////// remove pre test step data
+        signBizService.deleteAll();
     }
 
 
     @Order(2)
     @Test
     void signTransaction() {
+        // init param
+        SignParam signParam = new SignParam("002", LocalDateTime.now());
+        SignRetryParam signRetryParam = new SignRetryParam("002", LocalDateTime.now(), "{\"serialNo\":\"123\",\"userNo\":\"002\",\"signTime\":\"2022-11-03\",\"type\":\"01\"}", "01");
+        SignBizParam signBizParam = new SignBizParam(signParam, signRetryParam);
 
+        ////////////////////////test 1st case: save without transaction///////////////////////////
+        // save without transaction
+        signBizService.signTransaction(signBizParam, ConditionEnum.NORMAL, ConditionEnum.NORMAL, ConditionEnum.NORMAL);
+        // assert save result
+        Sign signCaseOne = signService.getOneByParam(signParam);
+        SignRetry signRetryCaseOne = signRetryService.getOneByParam(signRetryParam);
+        assertNotNull(signCaseOne);
+        assertNotNull(signRetryCaseOne);
+
+        /////////////////////////// remove pre test step data
+        signBizService.deleteAll();
+
+
+        ////////////////////////test 2nd case: save first service exception///////////////////////////
+        assertThrows(RuntimeException.class, () -> {
+            signBizService.signTransaction(signBizParam, ConditionEnum.NORMAL, ConditionEnum.RUNTIME_EXCEPTION, ConditionEnum.NORMAL);
+        });
+        Sign signCaseTwo = signService.getOneByParam(signParam);
+        SignRetry signRetryCaseTwo = signRetryService.getOneByParam(signRetryParam);
+        assertNull(signCaseTwo); // because of exception rollback
+        assertNull(signRetryCaseTwo); // not run because exception before this
+
+        /////////////////////////// remove pre test step data
+        signBizService.deleteAll();
+
+
+        ////////////////////////test 3rd case: save first service normal, second service exception///////////////////////////
+        assertThrows(RuntimeException.class, () -> {
+            signBizService.signTransaction(signBizParam, ConditionEnum.NORMAL, ConditionEnum.NORMAL, ConditionEnum.RUNTIME_EXCEPTION);
+        });
+        Sign signCaseThree = signService.getOneByParam(signParam);
+        SignRetry signRetryCaseThree = signRetryService.getOneByParam(signRetryParam);
+        assertNull(signCaseThree); // not save because of second service exception, both service use the same sql connection
+        assertNull(signRetryCaseThree); // not save successfully because of exception
+
+        /////////////////////////// remove pre test step data
+        signBizService.deleteAll();
+
+
+        ////////////////////////test 4th case: save both sub service normal, main method exception///////////////////////////
+        assertThrows(RuntimeException.class, () -> {
+            signBizService.signTransaction(signBizParam, ConditionEnum.RUNTIME_EXCEPTION, ConditionEnum.NORMAL, ConditionEnum.NORMAL);
+        });
+        Sign signCaseFour = signService.getOneByParam(signParam);
+        SignRetry signRetryCaseFour = signRetryService.getOneByParam(signRetryParam);
+        assertNull(signCaseFour);
+        assertNull(signRetryCaseFour);
+
+        /////////////////////////// remove pre test step data
+        signBizService.deleteAll();
     }
 }
